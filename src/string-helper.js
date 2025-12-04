cb4926f293bfe03c749bf8f423c0f7e0eec54c5b
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-12-04 13:03:40 +0000

    feat: reverseString functionality implemented

--- src/string-helper.js
+++ src/string-helper.js
@@ -5,4 +5,14 @@ function capitalise(text) {
     return text[0].toUpperCase() + text.slice(1);
 }
 
-export { capitalise };
+function reverseString(text) {
+    if (typeof text !== "string")
+        throw new Error(`${text} is not a valid string`);
+    arr = [];
+    for (let i = text.length - 1; i >= 0; i--) {
+        arr.push(text[i]);
+    }
+    return arr.join("");
+}
+
+export { capitalise, reverseString };
--- src/string-helper.spec.js
+++ src/string-helper.spec.js
@@ -1,4 +1,4 @@
-import { capitalise } from "./string-helper";
+import { capitalise, reverseString } from "./string-helper";
 
 describe("Test Capitalise", () => {
     test("capitalise exists", () => {
@@ -24,4 +24,24 @@ describe("Test Capitalise", () => {
             }).toThrow();
         }
     );
+});
+
+describe("Reverse string", () => {
+    test("reverse string exists", () => {
+        expect(reverseString).toBeDefined();
+    });
+
+    test.each([
+        { text: "hello", output: "olleh" },
+        { text: "hello World", output: "dlroW olleh" },
+        { text: "", output: "" },
+    ])("reverses string - $text", ({ text, output }) => {
+        expect(reverseString(text)).toMatch(output);
+    });
+
+    test.each([0, false, 4.2])("Errors on non-strings - %s", (text) => {
+        expect(() => {
+            reverseString(text);
+        }).toThrow();
+    });
 });
