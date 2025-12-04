a1703b476d19bc594f31139fbdab225be386006a
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-12-04 13:16:00 +0000

    refactor: string check separated

--- src/string-helper.js
+++ src/string-helper.js
@@ -1,13 +1,16 @@
-function capitalise(text) {
+function throwIfNotString(text) {
     if (typeof text !== "string")
         throw new Error(`${text} is not a valid string`);
+}
+
+function capitalise(text) {
+    throwIfNotString(text);
     if (text === "") return "";
     return text[0].toUpperCase() + text.slice(1);
 }
 
 function reverseString(text) {
-    if (typeof text !== "string")
-        throw new Error(`${text} is not a valid string`);
+    throwIfNotString(text);
     arr = [];
     for (let i = text.length - 1; i >= 0; i--) {
         arr.push(text[i]);
